2cd3a608942be624cceaa0d122039806d0a5db2b
Tatarize <tatarize@gmail.com>
2022-03-07 10:24:44 -0800

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="vpype-vectrace",
-    version="0.1.0",
+    version="0.1.1",
     description="vpype polygon tracer plugin",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -14,12 +14,15 @@ setup(
     author_email="tatarize@gmail.com",
     url="https://github.com/tatarize/vpype-vectrace/",
     packages=["vpype_vectrace"],
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
-    ),
+    ],
     install_requires=[
         "click",
         "vpype>=1.9,<2.0",
